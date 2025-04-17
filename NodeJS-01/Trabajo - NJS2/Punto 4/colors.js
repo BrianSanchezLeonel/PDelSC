77dfafe9661c3ff9fe5bee735d471f764b2e3d56
Brian <130560406+BrianSanchezLeonel@users.noreply.github.com>
2025-04-17 07:34:47 -0300

    Add files via upload

---
+++ NodeJS-01/Trabajo - NJS2/Punto 4/colors.js
@@ -0,0 +1,6 @@
+const colors = require('colors');
+
+console.log("Este texto es verde".green);
+console.log("Esto es rojo".red);
+console.log("Texto en amarillo".yellow);
+console.log("Texto azul con fondo blanco".blue.bgWhite);
