b1890443ae6cb6c00fee48c0ddb0993f47be2270
Brian <130560406+BrianSanchezLeonel@users.noreply.github.com>
2025-04-17 07:33:24 -0300

    Add files via upload

---
+++ NodeJS-01/Trabajo - NJS2/Punto 2/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Error interno del servidor');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
+});
+
+// Iniciar el servidor
+server.listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
