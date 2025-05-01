dcf086b3835eee7b9d0716f18ad30698fafaa871
Brian Sanchez <briansanchezleonel@gmail.com>
2025-05-01 17:04:10 -0300

    Ejercicios

--- NodeJS-01/Script2.js
+++
@@ -1,9 +0,0 @@
-var suma = 4 + 5;
-var resta = 3 - 6;
-var multiplicacion = 2 * 7;
-var division = 20 / 4;
-
-console.log("La suma entre 4 y 5 es: ", suma);
-console.log("La resta entre 3 y 6 es: ", resta);
-console.log("La multiplicacion entre 2 y 7 es: ", multiplicacion);
-console.log("La division entre 20 y 4 es: ", division);
--- NodeJS-01/Script4-2.js
+++
@@ -1,6 +0,0 @@
-import{suma, resta, multiplicacion, division} from "./Script4-1.js";
-
-console.log("La suma entre 5 y 3: " + suma(5, 3));
-console.log("La resta entre 8 y 6: " + resta(8, 6));
-console.log("La multiplicacion entre 3 y 11: " + multiplicacion(3, 11));
-console.log("La division entre 30 y 5: " + division(30, 5));
